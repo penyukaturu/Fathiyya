4881788f85373f548d094836e22afe2c7ccfc503
penyukaturu <Fathiyyaarsya@gmail.com>
2025-04-24 15:11:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("Cara turu nyenyak tanpa ketindihan")
 st.write(
     "Beragama, meyakini bahwa tuhan ada & berdoa sebelum tidur. (jgn lupa niatnya tidur ya, bukan nguli 60 soal mtk"
 )
+
+st.image("viewhttps://github.com/penyukaturu/Fathiyya/blob/main/19431362bebf8918d2d215538839cbb6.gif
