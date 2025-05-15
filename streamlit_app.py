a75727772c6d92dc80243ed64e91d5e56295e024
penyukaturu <Fathiyyaarsya@gmail.com>
2025-05-15 15:15:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ st.write(
     "Beragama, meyakini bahwa tuhan ada & berdoa sebelum tidur. (jgn lupa niatnya tidur ya, bukan nguli 60 soal mtk")
 
 st.image("64c5f432fdf27f66506429bfb56f7ca2.jpg") 
+st.image("8ff452acf79a743fe577576c29ab4551.jpg") 
+
+st.write(
+    "Udah ah, cape") 
