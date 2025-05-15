6873ff1d52453ed469db41db59c60ba62febd369
penyukaturu <Fathiyyaarsya@gmail.com>
2025-05-15 14:37:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 st.title("Cara turu nyenyak tanpa ketindihan")
 st.write(
-    "Beragama, meyakini bahwa tuhan ada & berdoa sebelum tidur. (jgn lupa niatnya tidur ya, bukan nguli 60 soal mtk"
-)
+    "Beragama, meyakini bahwa tuhan ada & berdoa sebelum tidur. (jgn lupa niatnya tidur ya, bukan nguli 60 soal mtk")
 
-st.image("view/64c5f432fdf27f66506429bfb56f7ca2.jpg")
+st.image"view/64c5f432fdf27f66506429bfb56f7ca2.jpg"
